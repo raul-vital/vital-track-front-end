bffd58ad97a0ed1dccda5dceedc1b38675612ba2
raul-vital <raul.rsv@hotmail.com>
2024-12-05 17:18:17 -0800

    add ProgressForm.module.css and WorkoutDetails.module.css with styling

--- src/components/ProgressForm/ProgressForm.jsx
+++ src/components/ProgressForm/ProgressForm.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import * as workoutService from '../../services/workoutService'
+import styles from './ProgressForm.module.css'
 
 const ProgressForm = ({ workout, handleAddProgress}) => {
     const [formData, setFormData] = useState({
@@ -23,16 +24,19 @@ const ProgressForm = ({ workout, handleAddProgress}) => {
     }
 
     return(
-        <>
+        
+        <main className={styles.container}>
         <section>
-        <div>
-            {console.log(workout.progress)}
-             {!workout.progress.length && <p>There isn't any progress saved.</p>}
+        <div className={styles.cardDiv}>
+             {!workout.progress.length && <p className={styles.noProgress}>There isn't any progress saved.</p>}
             {workout.progress.map((progressData)=> (
-                <div key={progressData._id}>
-                    <p>Date: {progressData.date}</p>
-                    <p>Weights Lifted: {progressData.weightsLifted}</p>
-                    <p>Notes: {progressData.notes || 'No notes provided.'}</p>
+                <div className={styles.cardLi} key={progressData._id}>
+                    <h3>Date:</h3>
+                    <p>{progressData.date}</p>
+                    <h3>Weights Lifted:</h3>
+                    <p>{progressData.weightsLifted} Lb(s).</p>
+                    <h3>Notes:</h3>
+                    <p>{progressData.notes || 'No notes provided.'}</p>
                 </div>
             ))}
 
@@ -41,10 +45,12 @@ const ProgressForm = ({ workout, handleAddProgress}) => {
         </section>
         <section>
          <div>
+        <hr></hr>
+         <h1>Add Progress</h1>
         <form onSubmit={handleSubmit}>
-        
         <label htmlFor='date'> Date: </label>
         <input
+          required
           type="text"
           name="date"
           id="date"
@@ -62,18 +68,23 @@ const ProgressForm = ({ workout, handleAddProgress}) => {
           onChange={handleChange}
         />
          <label htmlFor='notes'> Notes: </label>
-        <textarea
+        <textarea 
           type="text"
           name="notes"
           id="notes"
           value={formData.notes}
           onChange={handleChange}
         />
-        <button type="submit">Submit</button>
+        <button className={styles.submitBtn} type="submit">Submit</button>
+        
         </form>
         </div>
+        <footer></footer>
         </section>
-        </>
+
+        </main>
+
+        
     )
 
 }
--- src/components/WorkoutDetails/WorkoutDetails.jsx
+++ src/components/WorkoutDetails/WorkoutDetails.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useContext } from 'react'
 import { useParams, Link } from "react-router-dom"
 import ProgressForm from '../ProgressForm/ProgressForm'
 import * as workoutService from '../../services/workoutService'
-
+import styles from './WorkoutDetails.module.css'
 
 const WorkoutDetails = (props) => {
     const {workoutId} = useParams()
@@ -27,32 +27,39 @@ const WorkoutDetails = (props) => {
    if(!workout) return <main>Loading...</main>
 
    return(
-    <main>
-        <div>
-        <header>
-            <h3>{workout.title}</h3>
+    <main className={styles.container}>
+        <h3>{workout.title}</h3>
+        <div className={styles.primaryDiv}>
+        <section>
+            <p>{new Date(workout.createdAt).toLocaleDateString()}</p>
+            <hr></hr>
+            <h4>Category:</h4>
             <p>{workout.category}</p>
-            <p>Sets: {workout.sets}</p>
-            <p>Reps: {workout.reps}</p>
-            <p>Weights: {workout.weight} {!workout.weight ? "No Weights Used." : "lb(s)"}</p>
+            <h4>Sets:</h4>
+            <p>{workout.sets}</p>
+            <h4>Reps:</h4>
+            <p>{workout.reps}</p>
+            <h4>Weights:</h4>
+            <p>{workout.weight} {!workout.weight ? "No Weights Used." : "lb(s)"}</p>
         {workout.user._id && (
             <>
             <button><Link to={`/workouts/${workoutId}/edit`}>Edit Workout</Link></button>
-            <button onClick={()=> props.handleRemoveWorkout(workoutId)}>Delete</button>
+            <button className={styles.deleteBtn} onClick={()=> props.handleRemoveWorkout(workoutId)}>Delete</button>
             </>
         )}
-        </header>
+        </section>
         </div>
 
-        <div>
+        <div className={styles.secondaryDiv}>
         <section>
             
             <h3> {workout.title} - Progress</h3>
             <div>
-                {isVisible && <ProgressForm handleAddProgress={handleAddProgress} workout={workout}/>}
-                <button onClick={()=>setIsVisible(!isVisible)}>
-                    Show Or Hide
+                <button className={styles.progressBtn} onClick={()=>setIsVisible(!isVisible)}>
+                    {!isVisible? 'Show Progress' : 'Hide Progress'}
                 </button>
+                {isVisible && <ProgressForm handleAddProgress={handleAddProgress} workout={workout}/>}
+                
              </div>
 
           </section>
