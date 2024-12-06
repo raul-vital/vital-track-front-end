0eb7af7b264b5e81e28d430ec20661a45f10636b
raul-vital <raul.rsv@hotmail.com>
2024-12-05 17:48:13 -0800

    add WorkoutList.module.css with styling

--- src/components/WorkoutList/WorkoutList.jsx
+++ src/components/WorkoutList/WorkoutList.jsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom'
+import styles from './WorkoutList.module.css'
 const WorkoutList = (props) =>{
     return(
-        <main>
-            <div>
-            {!props.workouts.length && <h3>No Workout Saved!</h3>}
+        <main className={styles.container}>
+            <div className={styles.cardDiv}>
+            {!props.workouts.length && <h1>No Workout Saved!</h1>}
             {props.workouts.map((workout) => (
                <Link key={workout._id} to={`/workouts/${workout._id}`}>
-                 <header>
+                 <div className={styles.cardLi}>
+                    <p>{new Date(workout.createdAt).toLocaleDateString()}</p>
                     <h2>{workout.category}</h2>
                     <h3>{workout.title}</h3>
-                    <p>{new Date(workout.createdAt).toLocaleDateString()}</p>
-                 </header>
+                    
+                 </div>
                 </Link>
             ))}
            </div> 
